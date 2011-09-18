779b3841c7d83cb35fc2ab95d53191160f248e79
Daniel Lauzon <daniel.lauzon@gmail.com>
2011-09-18 01:49:53 -0400

    moving ahead

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## ax-rq Render Queue
 
+  For starters both producers and consumers are in this one demo, although
+  the interface exported by Kue allows for deletion of jobs.
+  
--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var port = Number(process.env.VCAP_APP_PORT || 3000),
 var cf = require("cloudfoundry");
 var redisSvc = cf.redis["ax-rq-redis"]||{"credentials":{ 
      host: '127.0.0.1',
-     port: 6380
+     port: 6379
      }};
 
 var cred = redisSvc.credentials;
@@ -44,7 +44,7 @@ var jobs = kue.createQueue();
 // in your http processes upon
 // user input etc.
 
-function create() {
+function produce() {
   var name = ['tobi', 'loki', 'jane', 'manny'][Math.random() * 4 | 0];
   var job = jobs.create('video conversion', {
       title: 'converting ' + name + '\'s to avi'
@@ -67,16 +67,18 @@ function create() {
 
   job.save();
 
-  setTimeout(create, Math.random() * 20000 | 0);
+  setTimeout(produce, Math.random() * 30000 | 0);
 }
 
-create();
+produce();
 
 // process video conversion jobs, 1 at a time.
 
 jobs.process('video conversion', 1, function(job, done){
   var frames = job.data.frames;
 
+  console.log('\nstart processing job #'+job.id+'\n');
+  
   function next(i) {
     // pretend we are doing some work
     convertFrame(i, function(err){
@@ -84,7 +86,7 @@ jobs.process('video conversion', 1, function(job, done){
       // report progress, i/frames complete
       job.progress(i, frames);
       if (i >= frames) done()
-      else next(i + Math.random() * 10);
+      else next(i + Math.random() * 50);
     });
   }
 
@@ -100,3 +102,71 @@ kue.app.listen(port, host);
 
 console.log('process.env: '+util.inspect(process.env, false, null)+'\n\n');
 console.log('Kue Server running at http://' + host + ':' + port + '/');
+
+// cleanup some jobs
+var completeJobExpiry=5*60*1000; // 5 minutes
+function cleanup(){
+    console.log("cleaning up");
+    jobs.card('complete',function (err,n) {
+        if (err) return;
+        console.log("completed tasks: "+n);
+    });
+    jobs.state('complete',function (err,ids) {
+        if (err) return;
+        (ids).forEach(function(id,index) {
+            console.log("considering task: "+id);
+            kue.Job.get(id,function(err,job) {
+                if (err) {
+                    console.error('remove failed to find:',id,err);
+                    return;
+                }
+                if (!job){
+                    console.log('!job,err',job,err);
+                    return;
+                }
+                /*var clone = {
+                    type:job.type,
+                    data:job.data,
+                    id:job.id,
+                    created_at:job.created_at,
+                    updated_at:job.updated_at
+                }
+                console.log(clone);*/
+                var updated_at = parseInt(job.updated_at, 10),
+                    howOld = updated_at - Date.now(),
+                    shouldDelete = ! Math.max(updated_at + completeJobExpiry - Date.now(), 0);
+
+
+                if (!shouldDelete) {
+                    console.log("-not removing task: "+id,howOld/60000,'min ago:',new Date(updated_at));
+                    return;
+                }
+                console.log("+removing task: "+id,howOld/60000,'min ago:',new Date(updated_at));
+                
+                //console.log('removing',id,job);
+                job.remove(function(err) {
+                    if (err){
+                        console.error('2-remove failed:',id,err);
+                    } else {
+                        console.log('confirmed that remove worked',id);
+                    }
+                })
+            });
+        });
+    });
+};
+
+//cleanup();
+//setTimeout(cleanup,200);
+setInterval(cleanup,3000);
+
+function myownaccess () {
+    var client = kue.redis.createClient();
+    
+    client.dbsize(function (err,n) {
+        if (err) return;
+        console.log("dbsize: ",n);
+    });
+    
+}
+//setTimeout(myownaccess,200);
